676964b1c8f67827228f858cecc8f76c78a602d5
Experience Agile <experience@qwan.eu>
2022-05-19 13:05:21 +0000

    Delivers can for price

--- src/main/java/eu/qwan/vendingmachine/VendingMachine.java
+++ src/main/java/eu/qwan/vendingmachine/VendingMachine.java
@@ -6,13 +6,25 @@ import java.util.Optional;
 
 public class VendingMachine {
     private Map<Choice, Can> choices = new HashMap<>();
+    private float price;
+    private float credits;
 
     public Optional<Can> deliver(Choice choice) {
         if (!choices.containsKey(choice)) return Optional.empty();
+        if (price != credits) return Optional.empty();
         return Optional.of(choices.get(choice));
     }
 
     public void configureChoice(Choice choice, Can can) {
+        configureChoice(choice, can, 0);
+    }
+
+    public void configureChoice(Choice choice, Can can, float price) {
+        this.price = price;
         choices.put(choice, can);
+    }
+
+    public void insert(float amount) {
+        credits = amount;
     }
 }
--- src/test/java/eu/qwan/vendingmachine/PricedVendingMachineTest.java
+++ src/test/java/eu/qwan/vendingmachine/PricedVendingMachineTest.java
@@ -9,27 +9,22 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-public class ConfiguredVendingMachineTest {
+public class PricedVendingMachineTest {
     private VendingMachine machine = new VendingMachine();
 
     @BeforeEach
     public void setup() {
-        machine.configureChoice(Choice.FIZZY_ORANGE, Can.FANTA);
-        machine.configureChoice(Choice.COLA, Can.COKE);
+        machine.configureChoice(Choice.FIZZY_ORANGE, Can.FANTA, 1.0f);
     }
 
     @Test
-    public void deliversNothingOnUnconfiguredChoice() {
-        assertThat(machine.deliver(Choice.BEER), is(equalTo(Optional.empty())));
+    public void deliversNothingWhenNotPaid() {
+        assertThat(machine.deliver(Choice.FIZZY_ORANGE), is(equalTo(Optional.empty())));
     }
 
     @Test
-    public void deliversCokeWhenChoosingCola() {
-        assertThat(machine.deliver(Choice.COLA), is(equalTo(Optional.of(Can.COKE))));
-    }
-
-    @Test
-    public void deliversFantaWhenChoosingFizzyOrange() {
+    public void deliversWhenNotPaidThePriceExactly() {
+        machine.insert(1.0f);
         assertThat(machine.deliver(Choice.FIZZY_ORANGE), is(equalTo(Optional.of(Can.FANTA))));
     }
 }
