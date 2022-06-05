73c2456bb87fc98d413558cc315db8e1f88f6f37
Felipe Flores <felipe.flores_external@wartsila.com>
2022-06-05 15:43:33 +0200

    Make CashRegister use cents

--- src/main/java/eu/qwan/vendingmachine/CashRegister.java
+++ src/main/java/eu/qwan/vendingmachine/CashRegister.java
@@ -1,22 +1,19 @@
 package eu.qwan.vendingmachine;
 
 public class CashRegister implements Cashier {
-    float credits;
+    int credits;
 
     public CashRegister() {
     }
 
     public void insert(int amount) {
-        var cents = (float) amount;
-        credits += cents / 100.0f;
+        credits += amount;
     }
 
     @Override
     public boolean checkout(int price) {
-        var cents = (float) price;
-        var cost = cents / 100.0f;
-        if (cost > credits) return false;
-        credits -= cost;
+        if (price > credits) return false;
+        credits -= price;
         return true;
     }
 }
